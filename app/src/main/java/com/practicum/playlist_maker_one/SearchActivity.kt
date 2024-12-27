48cb852ff20bde18c6af913429396724c36b764d
User <s.hlopovskih@gmail.com>
2024-12-27 20:55:11 +0300

    9 спринт (исправленный)

--- app/src/main/java/com/practicum/playlist_maker_one/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker_one/SearchActivity.kt
@@ -1,41 +1,40 @@
 package com.practicum.playlist_maker_one
 
+import android.content.Context
 import android.content.Intent
 import android.media.Image
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
-
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
-
     private var textInput : String? = null
     private lateinit var editedText: EditText
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search)) { v, insets ->
-
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
 
-        val closeButton = findViewById<ImageButton>(R.id.exitButtonSearch)
         editedText = findViewById(R.id.search_bar)
         val clearInput = findViewById<ImageView>(R.id.clearButton)
 
@@ -45,9 +44,7 @@ class SearchActivity : AppCompatActivity() {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                clearInput.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
-
-                textInput = s.toString()
+                clearInput.isVisible = !s.isNullOrEmpty()
             }
 
             override fun afterTextChanged(s: Editable?) {}
@@ -55,13 +52,12 @@ class SearchActivity : AppCompatActivity() {
 
         clearInput.setOnClickListener{
             editedText.setText("")
+            hideKeyboard(it)
         }
 
-
-        closeButton.setOnClickListener{
+        findViewById<MaterialToolbar>(R.id.searchToolbar).setNavigationOnClickListener {
             finish()
         }
-
         if (savedInstanceState != null){
             editedText.setText(savedInstanceState.getString("searchText"))
         }
@@ -73,6 +69,10 @@ class SearchActivity : AppCompatActivity() {
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         editedText.setText(savedInstanceState.getString("searchText"))
+    }
 
+    private fun hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
--- app/src/main/java/com/practicum/playlist_maker_one/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker_one/SettingsActivity.kt
@@ -2,7 +2,6 @@ package com.practicum.playlist_maker_one
 
 import android.content.Intent
 import android.media.Image
-
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
@@ -11,54 +10,53 @@ import android.widget.RelativeLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.android.material.textview.MaterialTextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings)) { v, insets ->
-
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-
-        findViewById<RelativeLayout>(R.id.list2).setOnClickListener {
+        findViewById<MaterialTextView>(R.id.share).setOnClickListener {
             val intent = Intent(Intent.ACTION_SEND).apply {
                 type = "text/plain"
                 putExtra(
                     Intent.EXTRA_TEXT,
-                    (R.string.linkDevelopment)
+                    getString(R.string.linkDevelopment)
                 )
             }
             startActivity(Intent.createChooser(intent, getString(R.string.share_app)))
         }
 
-        findViewById<RelativeLayout>(R.id.list3).setOnClickListener {
+        findViewById<MaterialTextView>(R.id.support).setOnClickListener {
             val intent = Intent(Intent.ACTION_SEND).apply {
                 type = "message/rfc822"
-                putExtra(Intent.EXTRA_EMAIL, (R.string.mail))
-                putExtra(Intent.EXTRA_SUBJECT, (R.string.subjectMessage))
-                putExtra(Intent.EXTRA_TEXT,(R.string.gratefulMessage))
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(R.string.mail))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subjectMessage))
+                putExtra(Intent.EXTRA_TEXT,getString(R.string.gratefulMessage))
             }
             startActivity(intent)
         }
 
-        findViewById<RelativeLayout>(R.id.list4).setOnClickListener {
+        findViewById<MaterialTextView>(R.id.usersAgree).setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW).apply {
                 data = Uri.parse(getString(R.string.linkAgreement))
             }
             startActivity(intent)
         }
 
-        val closeButton = findViewById<ImageButton>(R.id.button1)
-
-        closeButton.setOnClickListener{
+        findViewById<MaterialToolbar>(R.id.toolbarSettings).setNavigationOnClickListener {
             finish()
         }
 
