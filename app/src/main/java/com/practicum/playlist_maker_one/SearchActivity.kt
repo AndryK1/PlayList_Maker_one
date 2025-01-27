de98a52bd1b200a0759119c645cb5f1a9d11338f
User <s.hlopovskih@gmail.com>
2025-01-27 15:28:42 +0300

    Revert "спринт 11(первая версия)"
    This reverts commit cba4db3469fe558a4f76b8dd67208dad16ae1ea3.

--- app/src/main/java/com/practicum/playlist_maker_one/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker_one/SearchActivity.kt
@@ -28,7 +28,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-//
+
 class SearchActivity : AppCompatActivity() {
     private var textInput : String? = null
     private lateinit var editedText: EditText
