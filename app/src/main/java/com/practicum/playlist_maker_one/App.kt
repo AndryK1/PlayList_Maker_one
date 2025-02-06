73f22e852939c30dd37aa384307db35c255d5cdc
User <s.hlopovskih@gmail.com>
2025-02-06 14:39:50 +0300

    спринт 12
    Please enter the commit message for your changes. Lines starting
    Lines starting with '#' will be ignored, and an empty message aborts the commit.

---
+++ app/src/main/java/com/practicum/playlist_maker_one/App.kt
@@ -0,0 +1,25 @@
+package com.practicum.playlist_maker_one
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+const val APP_PREFERENCES = "AppPreferences"
+const val DARK_THEME_KEY = "dark_theme"
+const val HISTORY_KEY = "search_history_key"
+
+
+class App : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        val sharedPrefs = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
+        val isNightModeEnabled = sharedPrefs.getBoolean(DARK_THEME_KEY, false)
+
+        AppCompatDelegate.setDefaultNightMode(
+            if (isNightModeEnabled) AppCompatDelegate.MODE_NIGHT_YES
+            else AppCompatDelegate.MODE_NIGHT_NO
+        )
+    }
+
+
+}
