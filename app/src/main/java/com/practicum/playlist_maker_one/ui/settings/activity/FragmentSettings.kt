2ba61bd365f50c25a27b87c1b8033ffe18e2cf48
User <s.hlopovskih@gmail.com>
2025-07-10 03:43:08 +0300

    19 спринт

--- app/src/main/java/com/practicum/playlist_maker_one/ui/settings/activity/FragmentSettings.kt
+++ app/src/main/java/com/practicum/playlist_maker_one/ui/settings/activity/FragmentSettings.kt
@@ -3,72 +3,67 @@ package com.practicum.playlist_maker_one.ui.settings.activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import org.koin.androidx.viewmodel.ext.android.viewModel
-import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.switchmaterial.SwitchMaterial
-import com.google.android.material.textview.MaterialTextView
+import androidx.fragment.app.Fragment
 import com.practicum.playlist_maker_one.R
+import com.practicum.playlist_maker_one.databinding.FragmentSettingsBinding
 import com.practicum.playlist_maker_one.ui.settings.SettingsState
 import com.practicum.playlist_maker_one.ui.settings.view_model.SettingsViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
+class FragmentSettings : Fragment() {
 
-class SettingsActivity : AppCompatActivity() {
     private val viewModel : SettingsViewModel by viewModel()
+    private lateinit var binding: FragmentSettingsBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_settings)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
 
-        val themeSwitch = findViewById<SwitchMaterial>(R.id.darkTheme)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        viewModel?.observeState()?.observe(this){
+        viewModel?.observeState()?.observe(requireActivity()){
             if(it){
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                themeSwitch.isChecked = it
+                binding.darkTheme.isChecked = it
             }
             else{
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                themeSwitch.isChecked = it
+                binding.darkTheme.isChecked = it
             }
         }
 
-        viewModel?.observeSharingState()?.observe(this){
+        viewModel?.observeSharingState()?.observe(requireActivity()){
             render(it)
         }
         //тёмная тема
 
-        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+        binding.darkTheme.setOnCheckedChangeListener { _, isChecked ->
             viewModel?.switchDarkTheme(isChecked)
         }
 
         //
-        findViewById<MaterialTextView>(R.id.share).setOnClickListener {
-           viewModel?.onShareClicked()
+        binding.share.setOnClickListener {
+            viewModel?.onShareClicked()
         }
 
-        findViewById<MaterialTextView>(R.id.support).setOnClickListener {
+        binding.support.setOnClickListener {
             viewModel?.onSupportClicked()
         }
 
-        findViewById<MaterialTextView>(R.id.usersAgree).setOnClickListener {
+        binding.usersAgree.setOnClickListener {
             viewModel?.onAgreementClicked()
-        }
-
-        findViewById<MaterialToolbar>(R.id.toolbarSettings).setNavigationOnClickListener {
-            finish()
         }
 
 
@@ -106,5 +101,4 @@ class SettingsActivity : AppCompatActivity() {
         }
 
     }
-
 }
